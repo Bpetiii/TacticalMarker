2a9289b23fd6dc19ab282d3e289a7ce6af4adbc9
Bpetiii <87673385+Bpetiii@users.noreply.github.com>
2024-10-21 19:29:14 +0200

    ---

---
+++ src/main/java/biraw/online/b_s_TacticalMarker/B_s_TacticalMarker.java
@@ -0,0 +1,17 @@
+package biraw.online.b_s_TacticalMarker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class B_s_TacticalMarker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
